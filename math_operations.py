81b41a14f068181748d1b73da447876a37571f96
Antonella Schiavoni <antonellaschiavoni2@gmail.com>
2025-10-06 23:26:30 +0200

    Break function

--- math_operations.py
+++ math_operations.py
@@ -6,7 +6,7 @@ Provides basic arithmetic functions: addition, subtraction, multiplication, and 
 
 def add(a, b):
     """Add two numbers."""
-    return a + b
+    return a * b
 
 
 def subtract(a, b):
